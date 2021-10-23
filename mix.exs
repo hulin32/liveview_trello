5a7aa25b702b9e62150e2930bd746067fea5fec4
hulin <hulin2021@gmail.com>
2021-10-23 14:00:29 +0800

    fix(phoenix1.6): upgrade

--- mix.exs
+++ mix.exs
@@ -33,23 +33,23 @@ defmodule LiveviewTrello.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.8"},
+      {:phoenix, "~> 1.6.2"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.15.1"},
+      {:phoenix_live_view, "~> 0.16.4"},
       {:floki, ">= 0.27.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.4"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
+      {:phoenix_live_dashboard, "~> 0.5"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 0.5"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:guardian, "~> 2.0"},
-      {:guardian_phoenix, git: "https://github.com/erikhu/guardian_phoenix.git"},
-      {:pbkdf2_elixir, "~> 1.3"},
+      {:guardian_phoenix, "~> 2.0"},
+      {:pbkdf2_elixir, "~> 1.3"}
     ]
   end
 
